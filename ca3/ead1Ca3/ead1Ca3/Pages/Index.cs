74aed714fdca02a9606a03e9bdd549bb6845ef55
aaronoc1996 <43674505+aaronoc1996@users.noreply.github.com>
2021-12-13 20:48:23 +0000

    basic weather and json in c# classes

---
+++ ca3/ead1Ca3/ead1Ca3/Pages/Index.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using System.Threading.Tasks;
+using ead1Ca3.Shared;
+
+
+namespace ead1Ca3.Pages
+{
+   public partial class Index : ComponentBase
+    {
+        private Root Main;
+        private string ErrorMessage;
+        private string city = "Dublin";
+        private int cityId = 560743;
+        private async Task GetDataAsync()
+        {
+            try
+            {
+                string uri = "https://meta-weather.now.sh/api/location/"+ cityId +"/";
+                Main = await Http.GetJsonAsync<Root>(uri);
+                ErrorMessage = String.Empty;
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+            }
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetDataAsync();
+        }
+
+    }
+}
---
+++ ca3/ead1Ca3/ead1Ca3/ServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ead1Ca3
+{
+    public static class ServiceExtensions
+    {
+        public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            
+
+            var response = await httpClient.SendAsync(request);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.EnsureSuccessStatusCode();
+
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<T>(responseBytes, new JsonSerializerOptions { });
+        }
+    }
+}
